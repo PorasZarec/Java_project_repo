329c0ac1d0e10337f00bbd888f545d6f2c129822
cePORRAZar <116750047+PorasZarec@users.noreply.github.com>
2023-02-05 15:43:32 +0800

    None

--- Main.java
+++ Main.java
@@ -1,19 +1,33 @@
-import java.util.Scanner;
+//import java.util.Scanner;
+import javax.swing.JOptionPane;
+
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
 		
-		System.out.print("What is your name? ");
-		String name = scan.nextLine();
 		
-		System.out.print("What is your name? ");
-		int age = scan.nextInt();
-		scan.nextLine();
+		/*
+		 * String name = "Bro";
+		 * boolean result = name.equalIgnoreCase("Bro"); Ignore caps
+		 * int result = name.leght(); int length
+		 * char result = name.charAt(0); char index
+		 * int result = name.indexOf("B"); return int index
+		 * boolean result = name.isEmpty(); check if string is empty
+		 * String result = name.toUpperCase();
+		 * String result = name.toLowerCase(); 
+		 * String result = name.trim(); cls white space
+		 * String result = name.replace('o','a'); Bra
+		 * */
+		String name = JOptionPane.showInputDialog("Enter your name");
+		JOptionPane.showMessageDialog(null, "Hellow " + name);
+	
+		int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
+		JOptionPane.showMessageDialog(null, "You are " + age + " Years old");
+				
+		double height = Double.parseDouble(JOptionPane.showInputDialog("Enter your height"));
+		JOptionPane.showMessageDialog(null, "You are " + height + "cm tall");
 		
-		System.out.println("Your name is " + name);
 		
-		scan.close();
+		
 	}
 }
-	
